8cc7179d96a5f6980dcd1ac963759214c35b5b14
IsraelLozano <camoens1@outlook.com>
2024-05-01 11:53:51 -0500

    [ADD] Pagina Comite Administrativo fix initial

--- MIDIS.SGPVL.AppWeb/Program.cs
+++ MIDIS.SGPVL.AppWeb/Program.cs
@@ -88,10 +88,10 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    //app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
@@ -101,7 +101,7 @@ app.UseAuthorization();
 app.UseSession();
 
 //Llama al migrate del seed.
-//await app.MigrateDatabaseAsync();
+await app.MigrateDatabaseAsync();
 
 app.MapControllerRoute(
     name: "default",
